59d8622e65d8c6255283022262f5f6b2e25bc399
David Lopez <ldavid.x.lopezl@gmail.com>
2019-11-20 19:21:17 -0700

    Add routes for endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      scope :merchants do
+        get '/find', to: 'merchants/find#show'
+        get '/find_all', to: 'merchants/find#index'
+        get '/random', to: 'merchants/random#show'
+      end
+      resources :merchants, only: [:index, :show]
+    end
+  end
 end
