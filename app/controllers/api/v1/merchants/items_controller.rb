f3b19d957c213f158830e44e0a7f60a5a5e73fe7
David Lopez <ldavid.x.lopezl@gmail.com>
2019-12-01 16:25:19 -0700

    Add all queries and endpoints

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Merchants::Relationships::InvoicesController < ApplicationController
+class Api::V1::Merchants::ItemsController < ApplicationController
   def index
     render json: which_serializer.new(which_resource.where(merchant_id: params['id']))
   end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,14 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :items
+  has_many :transactions, through: :items
+
+  def self.most_revenue(limit)
+    joins(invoice_items: :transactions).
+    select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as rev").
+    group(:id).
+    merge(Transaction.successful).
+    order(rev: :desc).limit(limit)
+  end
 end
